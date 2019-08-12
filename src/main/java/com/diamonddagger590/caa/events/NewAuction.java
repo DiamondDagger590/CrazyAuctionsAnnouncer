1d8bd10759d4f3411aa61ea19a02c1499d06219c
DiamondDagger590 <diamonddagger590@gmail.com>
2019-08-11 22:41:41 -0500

    Temp push

--- src/main/java/com/diamonddagger590/caa/events/NewAuction.java
+++ src/main/java/com/diamonddagger590/caa/events/NewAuction.java
@@ -36,9 +36,9 @@ public class NewAuction implements Listener{
 		else{
 			displayName = item.getItemMeta().getDisplayName();
 		}
-		serverMessage = com.diamonddagger590.caa.util.Methods.translateMessage(serverMessage, p, bid, item.getAmount(), itemType, ShopType.BID.getName(), displayName);
+		serverMessage = com.diamonddagger590.caa.util.Methods.translateMessage(serverMessage, p, bid, item.getAmount(), itemType, auctionType, displayName);
 		String discordMessage = CrazyAuctionsAnnouncer.getConfigFile().getString("Discord.Messages.AuctionStart");
-		discordMessage = com.diamonddagger590.caa.util.Methods.translateMessage(discordMessage, p, bid, item.getAmount(), itemType, ShopType.BID.getName(), displayName);
+		discordMessage = com.diamonddagger590.caa.util.Methods.translateMessage(discordMessage, p, bid, item.getAmount(), itemType, auctionType, displayName);
 		String displayType = CrazyAuctionsAnnouncer.getConfigFile().getString("Settings.MessageSendTo");
 		if((!CrazyAuctionsAnnouncer.getConfigFile().getBoolean("Settings.UseAnnouncementLimit")) || AnnouncerLimiter.canAnnounce()) {
 			if(displayType.equalsIgnoreCase("both") || displayType.equalsIgnoreCase("server")) {
