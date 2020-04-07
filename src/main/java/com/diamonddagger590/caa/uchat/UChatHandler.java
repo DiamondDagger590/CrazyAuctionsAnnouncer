b5ead374e6cbe10baf40835f15b29821e5afe76d
BadBones69 <joewojcik14@gmail.com>
2020-04-07 01:39:11 -0400

    Can't check if a String equals a string list.

--- src/main/java/com/diamonddagger590/caa/uchat/UChatHandler.java
+++ src/main/java/com/diamonddagger590/caa/uchat/UChatHandler.java
@@ -10,7 +10,7 @@ public class UChatHandler {
 	public static void sendMessage(String chan, String msg) {
 		if(Bukkit.getPluginManager().isPluginEnabled("UChat") || Bukkit.getPluginManager().isPluginEnabled("UltimateChat")){
 			for(UCChannel channel : UChat.get().getAPI().getChannels()) {
-				if(channel.getDiscordChannelID().equals(chan)) {
+				if(channel.getDiscordChannelID().contains(chan)) {
 					channel.sendMessage(Bukkit.getConsoleSender(), msg);
 					return;
 				}
